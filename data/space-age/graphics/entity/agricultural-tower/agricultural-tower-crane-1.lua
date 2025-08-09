3891f7fa735c00b13b192f07181dc33a30f68044
Ingo_Igel <224564910+Ingo-Igel@users.noreply.github.com>
2025-08-09 19:43:34 +0200

    first commit

---
+++ data/space-age/graphics/entity/agricultural-tower/agricultural-tower-crane-1.lua
@@ -0,0 +1,12 @@
+return
+{
+  width = 406*2,
+  height = 330*2,
+  shift = util.by_pixel( 0.0, -4.5),
+  line_length = 8,
+  filenames = {
+    "-1.png",
+    "-2.png",
+  },
+  lines_per_file = 8,
+}
---
+++ data/space-age/graphics/entity/electromagnetic-plant/electromagnetic-plant-lights-rotate.lua
@@ -0,0 +1,12 @@
+return
+{
+  width = 204*2,
+  height = 300*2,
+  shift = util.by_pixel( 4.0, -12.0),
+  line_length = 8,
+  filenames = {
+    "-1.png",
+    "-2.png",
+  },
+  lines_per_file = 7,
+}
